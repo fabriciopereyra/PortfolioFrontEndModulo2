b25843482ae8e18f5f0e8e9e12f1ff4a553fab21
PEREYRA ESPECHE, Fabricio Daniel <fabriciopereyra@gmail.com>
2023-03-12 21:07:08 -0300

    Seccion Habilidades y Projectos

---
+++ assets/script/app.js
@@ -0,0 +1,45 @@
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.background = "#3A3E4B";
+        document.getElementById("aside").style.visibility = "hidden";
+    } else {
+        document.getElementById("navbar").style.background = "none";
+        document.getElementById("aside").style.visibility = "visible";
+    }
+}
+
+const sectionSkill = document.getElementById('section-skill');
+const sectionProject = document.getElementById('section-project');
+const progressBars = document.querySelectorAll('.progress-bar');
+
+function showProgress() {
+    progressBars.forEach(progressBar => {
+        const value = progressBar.dataset.progress;
+        progressBar.style.opacity = 1;
+        progressBar.style.width = `${value}%`;
+    });
+}
+
+function hideProgress() {
+    progressBars.forEach(progressBar => {
+        const value = progressBar.dataset.progress;
+        progressBar.style.opacity = 0;
+        progressBar.style.width = 0;
+    });
+}
+
+window.addEventListener('scroll', () => {
+    const sSkill = sectionSkill.getBoundingClientRect().top;
+    const sProject = sectionProject.getBoundingClientRect().top;
+    const screen = window.innerHeight;
+    if ( sProject < screen ) {
+        showProgress();
+    } else {
+        hideProgress();
+    }
+    if (sSkill < 0 ) {
+        hideProgress();
+    }
+});
